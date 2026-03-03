b7f807405c3b92ffec3ce662aea992fcba37808d
izabelap28 <izabela.parizotto@escola.pr.gov.br>
2026-03-03 15:31:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,35 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let convidados = ["Ana","Carlos","Amanda","Bruno","Aline","Fernanda","Rafael","Alex"];
   const resultado = document.getElementById("resultado");
-  const input = document.getElementById("novoNome");
-
-  function atualizarLista() {
-    resultado.innerHTML = "<strong>Lista Atual:</strong><br>" + convidados.join("<br>");
-  }
-
-  document.getElementById("btnAdicionar").addEventListener("click", () => {
-    let nome = input.value.trim();
-    if(nome !== ""){
-      convidados.push(nome);
-      atualizarLista();
-      input.value = "";
-    }
-  });
-
-  document.getElementById("btnRemover").addEventListener("click", () => {
-    let nome = input.value.trim();
-    convidados = convidados.filter(n => n.toLowerCase() !== nome.toLowerCase());
-    atualizarLista();
-    input.value = "";
-  });
 
   document.getElementById("btnMaiusculo").addEventListener("click", () => {
     resultado.innerHTML = convidados.map(n => n.toUpperCase()).join("<br>");
   });
 
   document.getElementById("btnA").addEventListener("click", () => {
-    let contador = convidados.filter(n => n[0].toUpperCase() === "A").length;
-    resultado.textContent = "Quantidade com A: " + contador;
+    let total = convidados.filter(n => n[0].toUpperCase() === "A").length;
+    resultado.textContent = "Quantidade com A: " + total;
   });
 
   document.getElementById("btnLongos").addEventListener("click", () => {
@@ -40,15 +19,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.getElementById("btnOrdenar").addEventListener("click", () => {
     convidados.sort();
-    atualizarLista();
+    resultado.innerHTML = convidados.join("<br>");
   });
 
   document.getElementById("btnTotal").addEventListener("click", () => {
     resultado.textContent = "Total de convidados: " + convidados.length;
   });
 
-  document.getElementById("btnLimpar").addEventListener("click", () => {
-    resultado.innerHTML = "";
+  document.getElementById("btnInverter").addEventListener("click", () => {
+    convidados.reverse();
+    resultado.innerHTML = convidados.join("<br>");
   });
 
 });
