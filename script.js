b6d9b631f62b9333e5614238f692368fcf7e89e3
izabelap28 <izabela.parizotto@escola.pr.gov.br>
2026-03-03 16:01:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   let convidados = ["Ana","Carlos","Amanda","Bruno","Aline","Fernanda","Rafael","Alex"];
-  const resultado = document.getElementById("resultado");
+
+  const lista = document.getElementById("lista");
+  const totalCard = document.getElementById("totalCard");
+  const aCard = document.getElementById("aCard");
+  const longosCard = document.getElementById("longosCard");
+
+  function atualizarTela(){
+    lista.innerHTML = "";
+
+    convidados.forEach(nome => {
+      let li = document.createElement("li");
+      li.textContent = nome;
+      lista.appendChild(li);
+    });
+
+    totalCard.textContent = convidados.length;
+    aCard.textContent = convidados.filter(n => n[0].toUpperCase() === "A").length;
+    longosCard.textContent = convidados.filter(n => n.length > 5).length;
+  }
 
   document.getElementById("btnMaiusculo").addEventListener("click", () => {
-    resultado.innerHTML = convidados.map(n => n.toUpperCase()).join("<br>");
-  });
-
-  document.getElementById("btnA").addEventListener("click", () => {
-    let total = convidados.filter(n => n[0].toUpperCase() === "A").length;
-    resultado.textContent = "Quantidade com A: " + total;
-  });
-
-  document.getElementById("btnLongos").addEventListener("click", () => {
-    let lista = convidados.filter(n => n.length > 5);
-    resultado.innerHTML = lista.join("<br>");
+    convidados = convidados.map(n => n.toUpperCase());
+    atualizarTela();
   });
 
   document.getElementById("btnOrdenar").addEventListener("click", () => {
     convidados.sort();
-    resultado.innerHTML = convidados.join("<br>");
-  });
-
-  document.getElementById("btnTotal").addEventListener("click", () => {
-    resultado.textContent = "Total de convidados: " + convidados.length;
+    atualizarTela();
   });
 
   document.getElementById("btnInverter").addEventListener("click", () => {
     convidados.reverse();
-    resultado.innerHTML = convidados.join("<br>");
+    atualizarTela();
   });
+
+  atualizarTela();
 
 });
