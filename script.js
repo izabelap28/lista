e7d51d49dfc59d9aedc8e3a7c71dbe5332365cfa
izabelap28 <izabela.parizotto@escola.pr.gov.br>
2026-03-03 15:08:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-function processarLista() {
+let convidados = ["Ana", "Carlos", "Amanda", "Bruno", "Aline", "Fernanda", "Rafael", "Alex"];
 
-    let convidados = ["Ana", "Carlos", "Amanda", "Bruno", "Aline", "Fernanda", "Rafael", "Alex"];
-
-    let nomesMaiusculos = "";
-    let contadorA = 0;
-    let maisDeCinco = [];
+function mostrarMaiusculos() {
+    let resultado = "";
 
     for (let i = 0; i < convidados.length; i++) {
+        resultado += convidados[i].toUpperCase() + "<br>";
+    }
 
-        nomesMaiusculos += convidados[i].toUpperCase() + "<br>";
+    document.getElementById("resultado").innerHTML = 
+        "<strong>Nomes em Maiúsculo:</strong><br>" + resultado;
+}
 
+function contarLetraA() {
+    let contador = 0;
+
+    for (let i = 0; i < convidados.length; i++) {
         if (convidados[i].startsWith("A")) {
-            contadorA++;
-        }
-
-        if (convidados[i].length > 5) {
-            maisDeCinco.push(convidados[i]);
+            contador++;
         }
     }
 
-    document.getElementById("resultado").innerHTML =
-        "<strong>Nomes em Maiúsculo:</strong><br>" + nomesMaiusculos +
-        "<br><strong>Quantidade que começam com A:</strong> " + contadorA +
-        "<br><br><strong>Nomes com mais de 5 letras:</strong><br>" + maisDeCinco.join("<br>");
+    document.getElementById("resultado").innerHTML = 
+        "<strong>Quantidade que começam com A:</strong> " + contador;
+}
+
+function nomesLongos() {
+    let lista = [];
+
+    for (let i = 0; i < convidados.length; i++) {
+        if (convidados[i].length > 5) {
+            lista.push(convidados[i]);
+        }
+    }
+
+    document.getElementById("resultado").innerHTML = 
+        "<strong>Nomes com mais de 5 letras:</strong><br>" + lista.join("<br>");
 }
